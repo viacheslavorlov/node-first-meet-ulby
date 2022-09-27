95ea226ac8ab1de55c89ce2201a2f9af14bd7d15
viacheslav <viacheslavorlov@gmail.com>
2022-09-27 23:58:20 +0300

    36 min https://www.youtube.com/watch?v=243pQXC5Ebs

--- index.js
+++ index.js
@@ -10,26 +10,28 @@ const crypto = require('crypto');
 console.log(chalk.red(text));
 console.log(__dirname, require)
 
+//* проверка многопоточности node.js
 
-const start = Date.now();
+// const start = Date.now();
 
-crypto.pbkdf2('123ttt', '5',
-	1000000, 64, 'sha512',
-	() => console.log('1 end ', Date.now() - start))
-crypto.pbkdf2('123ttt', '5',
-	1000000, 64, 'sha512',
-	() => console.log('2 end ', Date.now() - start))
-crypto.pbkdf2('123ttt', '5',
-	1000000, 64, 'sha512',
-	() => console.log('3 end ', Date.now() - start))
-crypto.pbkdf2('123ttt', '5',
-	1000000, 64, 'sha512',
-	() => console.log('4 end ', Date.now() - start))
-crypto.pbkdf2('123ttt', '5',
-	1000000, 64, 'sha512',
-	() => console.log('5 end ', Date.now() - start))
-crypto.pbkdf2('123ttt', '5',
-	1000000, 64, 'sha512',
-	() => console.log('6 end ', Date.now() - start))
+// crypto.pbkdf2('123ttt', '5',
+// 	1000000, 64, 'sha512',
+// 	() => console.log('1 end ', Date.now() - start))
+// crypto.pbkdf2('123ttt', '5',
+// 	1000000, 64, 'sha512',
+// 	() => console.log('2 end ', Date.now() - start))
+// crypto.pbkdf2('123ttt', '5',
+// 	1000000, 64, 'sha512',
+// 	() => console.log('3 end ', Date.now() - start))
+// crypto.pbkdf2('123ttt', '5',
+// 	1000000, 64, 'sha512',
+// 	() => console.log('4 end ', Date.now() - start))
+// crypto.pbkdf2('123ttt', '5',
+// 	1000000, 64, 'sha512',
+// 	() => console.log('5 end ', Date.now() - start))
+// crypto.pbkdf2('123ttt', '5',
+// 	1000000, 64, 'sha512',
+// 	() => console.log('6 end ', Date.now() - start))
 
 console.log(process.pid);
+console.log(process.env);
